1aad8e7f6a45d690cf97847efdcb6de8e3d85ceb
Christy Lam <cyyllam@gmail.com>
2020-04-16 21:38:29 -0700

    added video game boss constructor

--- lesson03/activity/index.js
+++ lesson03/activity/index.js
@@ -1,29 +1,58 @@
 // Create the Dog class constructor
-class Dog {
-	// Give it expected parameters
-	constructor(name, age, breed, color) {
-		// Set those equal to the instance
+// class Dog {
+// 	// Give it expected parameters
+// 	constructor(name, age, breed, color) {
+// 		// Set those equal to the instance
+// 		this.name = name;
+// 		this.age = age;
+// 		this.breed = breed;
+// 		this.color = color;
+// 		this.energyLevel = 0;
+// 		this.barkLevel = 10;
+// 	}
+// 	// pat method
+// 	pat() {
+// 		this.energyLevel++
+// 		this.barkLevel--;
+// 		console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and current bark level is ${this.barkLevel}.`);
+// 	}
+// }
+
+// // Create marty
+// const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
+
+// // Pat him 5 times
+// marty.pat();
+// marty.pat();
+// marty.pat();
+// marty.pat();
+// marty.pat();
+
+//----------------------------------------
+//video game character class constructor
+class videoGameBoss {
+	constructor(name, species, color, weapon) {
 		this.name = name;
-		this.age = age;
-		this.breed = breed;
+		this.species = species;
 		this.color = color;
-		this.energyLevel = 0;
-		this.barkLevel = 10;
+		this.weapon = weapon;
+		this.attackPower = 5;
+		this.healPower = 10;
 	}
-	// pat method
-	pat() {
-		this.energyLevel++
-		this.barkLevel--;
-		console.log(`This dog's name is ${this.name}, age is ${this.age}, current energy level is ${this.energyLevel} and current bark level is ${this.barkLevel}.`);
+	//attack & heal
+	fight() {
+		this.attackPower++;
+		this.healPower--;
+		console.log(`This boss is ${this.name}, weapon is ${this.weapon}, attack power is ${this.attackPower}, healing power is ${this.healPower}.`)
 	}
 }
 
-// Create marty
-const marty = new Dog('Marty', 6, 'Mutt', 'Brown');
+const attackTitan = new videoGameBoss("Eren", "Nine Titans", "Green", "Fists")
 
-// Pat him 5 times
-marty.pat();
-marty.pat();
-marty.pat();
-marty.pat();
-marty.pat();
+attackTitan.fight()
+attackTitan.fight()
+attackTitan.fight()
+attackTitan.fight()
+attackTitan.fight()
+attackTitan.fight()
+attackTitan.fight()
