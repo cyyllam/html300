c2e4a05163a0d5a1e0c2b4a2ec3c2254d986cba3
Christy Lam <cyyllam@gmail.com>
2020-04-18 21:57:35 -0700

    initialized functioning code, added scss content

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -55,70 +55,47 @@ let objPeople = [
 
 objPeople.forEach(element => console.log(element));
 
-// create function for first level divs
-function createDiv(aTarget, className) {
-    const target = $(aTarget);
-    target.append(`<div class=${className}></div>`);
-}
-
-function createSectionHeadshot(aTarget, className, imgSrc, imgAlt, name, title) {
-    const target = $(aTarget);
-    target.append(`<section class=${className}></section>`)
-    const newTarget = $(`.${className}`)
-    newTarget.append(`<img src=${imgSrc} alt=${imgAlt}>`);
-    newTarget.append(`<p>${name}</p>`);
-    newTarget.append(`<p>${title}</p>`);
-}
-function createSectionDetails(aTarget, className, articleClassName, divClassName) {
-    const target = $(aTarget);
-    target.append(`<section class=${className}></section>`);
-    const newTarget = $(`.${className}`);
-    newTarget.append(`<article class=${articleClassName}></article>`);
-    createArticleDetails(articleClassName, divClassName);
-}
-
-function createArticleDetails(aTarget, divClassName) {
-    const target = $(aTarget); //article
-    let aKey = null;
-    let aVal = null;
-    for (let i = 0; i < 5; i++) {
-        switch(i) {
-            case 0:
-                aKey = "Company";
-                aVal = "test";
-                break;
-            case 1:
-                aKey = "Experience";
-                aVal = "test";
-                break;
-            case 2:
-                aKey = "School";
-                aVal = "test";
-                break;
-            case 3:
-                aKey = "Major";
-                aVal = "test";
-                break;
-            case 4:
-                aKey = "Email";
-                aVal = "test";
-        };
-        createDiv(`.${aTarget}`, divClassName);
-        $(`article div:nth-child(${i+1}`).append(`<p>${aKey}</p>`);
-        $(`article div:nth-child(${i+1}`).append(`<p>${aVal}</p>`);
-    }
-}
-
-function createArticleLinkedIn(aTarget, className) {}
-
 $(document).ready(function(){
-    createDiv('.template-hook', "card-body");
-    createDiv('.card-body', "card-content");
-    createSectionHeadshot('.card-content', 
-    "card-content_headshot-container",
-    "imageSrc", "imageAlt", "name", "title");
-    createSectionDetails('.card-content',
-    "card-content_detail-container",
-    "attribute-container",
-    "attribute-container_inline");
+    objPeople.forEach(function(el){
+       let chunk = `<div class="card-body">
+        <div class="card-content">
+            <section class="card-content_headshot-container">
+                <img src="img/unsplash-headshot.jpg" alt="headshot of ${el.name}">
+                <p>${el.name}</p>
+                <p>${el.jobTitle}</p>
+            </section>
+            <section class="card-content_detail-container">
+                <article class="attribute-container">
+                    <div class="attribute-container_inline">
+                        <p>Company</p>
+                        <p>${el.company}</p>
+                    </div>
+                    <div class="attribute-container_inline">
+                        <p>Experience</p>
+                        <p>${el.experience}</p>
+                    </div>
+                    <div class="attribute-container_inline">
+                        <p>School</p>
+                        <p>${el.school}</p>
+                    </div>
+                    <div class="attribute-container_inline">
+                        <p>Major</p>
+                        <p>${el.major}</p>
+                    </div>
+                    <div class="attribute-container_inline">
+                        <p>Email</p>
+                        <p>${el.email}</p>
+                    </div>                                    
+                </article>
+                <article class="linkedin-container">
+                    <img src="img/linkedin.svg" alt="linked-in logo">
+                    <p>${el.linkedInUrl}</p>
+                </article>
+            </section>
+        </div>
+    </div>`;       
+
+    $('.template-hook').append(chunk);
+
+    }); 
 })
