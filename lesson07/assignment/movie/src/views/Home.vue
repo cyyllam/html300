1d7bea383bc27a078785096f5e0bcd63ed4ee445
Christy Lam <cyyllam@gmail.com>
2020-05-16 19:49:42 -0700

    initialize assignment 07

---
+++ lesson07/assignment/movie/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+  
+
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// create component called Landing.vue and import it
+// import Landing from @/components/Landing.vue
+
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
