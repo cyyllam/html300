6074fc9e3727a62a3d8d33b1c9782248af40860b
Christy Lam <cyyllam@gmail.com>
2020-05-18 19:37:17 -0700

    added image page content and photos

--- lesson07/assignment/movie/src/components/Images.vue
+++ lesson07/assignment/movie/src/components/Images.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-      <h2>This is the Images page</h2>
+      <b-container class="my-4">
+        <h2>Images</h2>
+        <b-row>
+          <!-- loop through each photo from the photos object, nesting img within a column -->
+          <b-col md="4" v-for="photo in photos" :key="photo.id">
+            <b-img thumbnail fluid :src="thumbUrl(photo.filename)" :alt="photo.altText"></b-img>
+          </b-col>
+        </b-row>
+      </b-container>
       
   </div>
 </template>
@@ -9,8 +17,21 @@
 export default {
   data () {
       return {
- 
+        // object containing image paths, alt text
+        photos: [
+          {filename: 'app-entertainment-ipad-mockup-265685.jpg', altText: 'Ipad with Netflix'},
+          {filename: 'black-and-red-samsung-galaxy-smartphone-1790556.jpg', altText: 'Photo by Paul Volkmer from Pexels'},
+          {filename: 'black-and-white-production-scene-take-tool-918281.jpg', altText: 'Photo by Obregonia D. Toretto from Pexels'},
+          {filename: 'food-snack-popcorn-movie-theater-33129.jpg', altText: 'Movie theatre popcorn'},
+          {filename: 'hollywood-sign-2671089.jpg', altText: 'Hollywood sign from Pexels'},
+          {filename: 'people-sitting-on-gang-chairs-2774556.jpg', altText: 'Photo by Luis Quintero from Pexels'},
+          ]
       }
+  },
+  methods: {
+    thumbUrl(filename) {
+      return require(`../assets/${filename}`);
+    },
   }
 }
 </script>
