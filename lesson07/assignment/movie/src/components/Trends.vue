49beb10fd1dab4321ec3c2a6e9c6ff6b07d29ea0
Christy Lam <cyyllam@gmail.com>
2020-05-19 22:24:05 -0700

    added trendstile component, toggle button to switch layout

--- lesson07/assignment/movie/src/components/Trends.vue
+++ lesson07/assignment/movie/src/components/Trends.vue
@@ -1,26 +1,52 @@
 <template>
   <div class="my-5">
-    <!-- this component references another component -->
+    <!-- this component references other components -->
+    <b-container>
+      <b-row align-h="center">
+        <b-button :pressed.sync="myToggle" class="mr-4 mb-4" variant="info">Toggle View</b-button>
+      </b-row>
+    </b-container>
+    
     <!-- the TrendsBlock component consists of a card for a graph or image and a side caption for 
     detailed text and explanation -->
-    <!-- generate as many blocks based on numBLocks variable -->
+    <!-- generate as many blocks or tiles based on numBLocks variable -->
+    <!-- default, if block button selected -->
+    <div v-if="myToggle">
       <TrendsBlock v-for="d in numBlocks" :key="d.id"/>
+    </div>
+      
+    <!-- if gallery button clicked   -->
+    <div>
+       <b-container>
+        <b-card-group deck>
+        <TrendsTile v-for="d in numBlocks" :key="d.id"/>
+        </b-card-group> 
+      </b-container>
+    </div>
+     
+      
   </div>
 </template>
 
 <script>
 // create component called Landing.vue and import it
 import TrendsBlock from '@/components/TrendsBlock.vue'
+import TrendsTile from '@/components/TrendsTile.vue'
 
 export default {
   name: 'Trends',
   components: {
-    TrendsBlock
+    TrendsBlock,
+    TrendsTile
   },
    data () {
       return {
-        numBlocks: 4
+        myToggle: true,
+        numBlocks: 10
       }
+  },
+  methods: {
+    
   }
 }
 </script>
